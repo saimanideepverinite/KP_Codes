e794ea019e20364da6363c6812c6ff086c6b108f
saimanideepverinite <90831870+saimanideepverinite@users.noreply.github.com>
2022-07-05 10:15:09 +0530

    Add files via upload

---
+++ multiModule/commons1/src/main/java/com/demo/Addition.java
@@ -0,0 +1,21 @@
+package com.demo;
+
+
+public class Addition {
+
+    public static int add(int a, int b){
+        System.out.println(a+b);
+        return  a+b;
+    }
+
+    public static int sub(int a, int b){
+        System.out.println(a*b);
+        return  a+b;
+    }
+}
+
+
+
+
+
+
---
+++ multiModule/service/src/main/java/com/example/ResultOfAdd.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import com.demo.Addition;
+
+
+public class ResultOfAdd {
+
+    public static int result(){
+//        System.out.println("in Result class");
+//        System.out.println(Addition.add(2,4));
+//        System.out.println("Substraction"+Addition.sub(2,3));
+        return Addition.add(2,3);
+    }
+
+}
